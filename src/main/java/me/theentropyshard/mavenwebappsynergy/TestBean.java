9d8c86a7eed172ec6e342e39f0257678440b8726
TheEntropyShard <theentropyshard@gmail.com>
2023-10-05 17:40:19 +0300

    Решение предыдущего ДЗ с помощью аннотаций

--- src/main/java/me/theentropyshard/mavenwebappsynergy/TestBean.java
+++ src/main/java/me/theentropyshard/mavenwebappsynergy/TestBean.java
@@ -1,10 +1,17 @@
 package me.theentropyshard.mavenwebappsynergy;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:values.properties")
 public class TestBean {
+    @Value("${hello.str}")
     private String name;
 
-    public TestBean(String name) {
-        this.name = name;
+    public TestBean() {
+
     }
 
     public String getName() {
--- src/main/java/me/theentropyshard/mavenwebappsynergy/TestSpring.java
+++ src/main/java/me/theentropyshard/mavenwebappsynergy/TestSpring.java
@@ -1,10 +1,10 @@
 package me.theentropyshard.mavenwebappsynergy;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("me.theentropyshard.mavenwebappsynergy");
         TestBean testBean = context.getBean("testBean", TestBean.class);
         System.out.println(testBean.getName());
         context.close();
