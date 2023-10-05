3b6197228a16007528fad6879b7896a52ea92352
TheEntropyShard <theentropyshard@gmail.com>
2023-10-05 16:36:44 +0300

    Spring Framework. Введение

---
+++ src/main/java/me/theentropyshard/mavenwebappsynergy/TestBean.java
@@ -0,0 +1,17 @@
+package me.theentropyshard.mavenwebappsynergy;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
